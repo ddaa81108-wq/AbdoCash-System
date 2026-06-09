bf50fe804dfc672c363aca4d860f5bcd5ac43666
ddaa81108-wq <ddaa81108@gmail.com>
2026-06-09 22:30:49 +0200

    Update service worker caching strategy and cache name

--- static/sw.js
+++ static/sw.js
@@ -1,20 +1,38 @@
-const CACHE_NAME = 'system-cache-v1';
+const CACHE_NAME = 'system-v1';
 
-self.addEventListener('install', (e) => {
-    e.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll([
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(cache =>
+            cache.addAll([
                 '/',
-                '/static/css/style.css',
-                '/static/js/main.js',
                 '/static/manifest.json'
-            ]);
-        })
+            ])
+        )
     );
 });
 
-self.addEventListener('fetch', (e) => {
-    e.respondWith(
-        fetch(e.request).catch(() => caches.match(e.request))
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(
+                keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+            )
+        )
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    if(event.request.method !== 'GET') return;
+
+    event.respondWith(
+        fetch(event.request)
+        .then(res => {
+            let clone = res.clone();
+            caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+            return res;
+        })
+        .catch(() => caches.match(event.request))
     );
 });
